b00269c483014585b2b5d5f7810728efb72dddae
Rich Lewis <rich@alternatefuture.uk>
2019-06-12 16:45:14 +0100

    Added path variable & clean up

--- Bingwallpaper.ps1
+++ Bingwallpaper.ps1
@@ -1,12 +1,20 @@
 $bingimagedata = Invoke-RestMethod -Uri "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB" -Method Get
 
-if (-not (Test-Path "$env:USERPROFILE\Pictures\BingWallpaper")) {
-    New-Item -ItemType Directory -Path "$env:USERPROFILE\Pictures\BingWallpaper"
-}
+$wallpaperdir = "$env:USERPROFILE\Pictures\BingWallpaper"
 
-Set-Location -Path "$env:USERPROFILE\Pictures\BingWallpaper"
+if (-not (Test-Path "$wallpaperdir")) {
+    New-Item -ItemType Directory -Path "$wallpaperdir"
+}
 
 $imagefilename = [System.Web.HttpUtility]::ParseQueryString(([uri]::new("http://www.bing.com$($bingimagedata.images[0].url)")).Query).Get("id")
 
-Invoke-WebRequest -Uri "https://www.bing.com$($bingimagedata.images[0].url)" -OutFile $imagefilename
-$bingimagedata.images[0] | Format-List title,copyright | out-file -FilePath "$imagefilename.txt"
+Invoke-WebRequest -Uri "https://www.bing.com$($bingimagedata.images[0].url)" -OutFile "$wallpaperdir\$imagefilename"
+$bingimagedata.images[0] | Format-List title,copyright | out-file -FilePath "$wallpaperdir\$imagefilename.txt"
+
+$wpfiles = Get-ChildItem -Path $wallpaperdir
+
+$wpfiles | ForEach-Object {
+    if ($_.CreationTimeUtc.CompareTo([DateTime]::UtcNow.AddDays(-3) -eq -1 ) ) {
+        $_ | Remove-Item
+    }
+}
