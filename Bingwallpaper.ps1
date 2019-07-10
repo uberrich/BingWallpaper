537073531a2acbfde306ba66f984db2783f3eb86
Rich Lewis <rich@alternatefuture.uk>
2019-07-10 15:46:12 +0100

    Add functionality to download missing days' files

--- Bingwallpaper.ps1
+++ Bingwallpaper.ps1
@@ -1,17 +1,26 @@
-$bingimagedata = Invoke-RestMethod -Uri "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB" -Method Get
-
 $wallpaperdir = "$env:USERPROFILE\Pictures\BingWallpaper"
 
 if (-not (Test-Path "$wallpaperdir")) {
     New-Item -ItemType Directory -Path "$wallpaperdir"
 }
 
-$imagefilename = [System.Web.HttpUtility]::ParseQueryString(([uri]::new("http://www.bing.com$($bingimagedata.images[0].url)")).Query).Get("id")
-
-Invoke-WebRequest -Uri "https://www.bing.com$($bingimagedata.images[0].url)" -OutFile "$wallpaperdir\$imagefilename"
-$bingimagedata.images[0] | Format-List title,copyright | out-file -FilePath "$wallpaperdir\$imagefilename.txt"
-
 $wpfiles = Get-ChildItem -Path $wallpaperdir
+
+$newestFileDate = ( $wpfiles | Sort-Object CreationTime | Select-Object -Last 1 ).CreationTime
+
+$daysToGet = [System.Math]::Round(([System.DateTime]::Now - $newestFileDate).TotalDays)
+
+if ($daysToGet -lt 1) {
+    $daysToGet = 1
+}
+
+$bingimagedata = Invoke-RestMethod -Uri "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=$daysToGet&mkt=en-GB" -Method Get
+
+$bingimagedata.images | ForEach-Object {
+    $imagefilename = [System.Web.HttpUtility]::ParseQueryString(([uri]::new("http://www.bing.com$($_.url)")).Query).Get("id")
+    Invoke-WebRequest -Uri "https://www.bing.com$($_.url)" -OutFile "$wallpaperdir\$imagefilename"
+    $_ | Format-List title,copyright | out-file -FilePath "$wallpaperdir\$imagefilename.txt"
+}
 
 while ($wpfiles.count -gt 10) {
     $wpfiles | Sort-Object CreationTime | Select-Object -First 2 | Remove-Item
