6de4cb985a3b1f39959061cbfc441904c7e1cc20
Rich Lewis <rich@alternatefuture.uk>
2019-07-15 22:14:20 +0100

    First stage of overlay

--- Bingwallpaper.ps1
+++ Bingwallpaper.ps1
@@ -1,4 +1,4 @@
-$wallpaperdir = "$env:USERPROFILE\Pictures\BingWallpaper"
+$wallpaperdir = "$env:USERPROFILE\Pictures\BingWallpaperTest"
 
 if (-not (Test-Path "$wallpaperdir")) {
     New-Item -ItemType Directory -Path "$wallpaperdir"
@@ -6,21 +6,37 @@ if (-not (Test-Path "$wallpaperdir")) {
 
 $wpfiles = Get-ChildItem -Path $wallpaperdir
 
-$newestFileDate = ( $wpfiles | Sort-Object CreationTime | Select-Object -Last 1 ).CreationTime
+if ($wpfiles -ne $null) {
+    $newestFileDate = ( $wpfiles | Sort-Object CreationTime | Select-Object -Last 1 ).CreationTime
 
-$daysToGet = [System.Math]::Round(([System.DateTime]::Now - $newestFileDate).TotalDays)
+    $daysToGet = [System.Math]::Round(([System.DateTime]::Now - $newestFileDate).TotalDays)
 
-if ($daysToGet -lt 1) {
-    $daysToGet = 1
+    if ($daysToGet -lt 1) {
+        $daysToGet = 1
+    }
+} else {
+    $daysToGet = 5
 }
+
+$sFormat = [System.Drawing.StringFormat]::new
+$sFormat.alignment = [System.Drawing.StringAlignment]::Far
 
 $bingimagedata = Invoke-RestMethod -Uri "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=$daysToGet&mkt=en-GB" -Method Get
 
 $bingimagedata.images | ForEach-Object {
     $imagefilename = [System.Web.HttpUtility]::ParseQueryString(([uri]::new("http://www.bing.com$($_.url)")).Query).Get("id")
     Invoke-WebRequest -Uri "https://www.bing.com$($_.url)" -OutFile "$wallpaperdir\$imagefilename"
-    $_ | Format-List title,copyright | out-file -FilePath "$wallpaperdir\$imagefilename.txt"
+    $imageText = $_ | Select-Object @{l='Title';e={$_.title}},@{l='Description';e={$_.copyright}}
+    
+    $imageText | Format-List | Out-File -FilePath "$wallpaperdir\$imagefilename.txt"
+
+    $imageFile = Get-Item -Path "$wallpaperdir\$imagefilename"
+    $bmp = [System.Drawing.Bitmap]::FromFile($imageFile)
+    $image = [System.Drawing.Graphics]::FromImage($bmp)
+    $SR = $bmp | Select-Object Width,Height
 }
+
+$wpfiles = Get-ChildItem -Path $wallpaperdir
 
 while ($wpfiles.count -gt 10) {
     $wpfiles | Sort-Object CreationTime | Select-Object -First 2 | Remove-Item
